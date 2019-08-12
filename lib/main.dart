58c45076925d5965f38315038251c1a1cbcb259a
Alexander <labianca.alex@gmail.com>
2019-08-12 07:23:28 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Test"),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/cuba.jpg'),
+              Text("Test Card")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
